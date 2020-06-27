5328222b877646e0c991e84f0a1b7fc2b8388e6d
Nicolas <nbelin@outlook.com>
2020-06-27 14:18:39 +0200

    Set window icon

--- 7WondersUI/main.cpp
+++ 7WondersUI/main.cpp
@@ -1,10 +1,13 @@
 #include "ui.hpp"
+#include "tools.hpp"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(Tools::imagePath("icon.png")));
     UI w;
     w.prepareMenu();
     w.show();
